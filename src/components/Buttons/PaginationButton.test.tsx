629d58cceb4811845b90b244d3f404fe84e69902
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-10 20:30:08 +0100

    PaginationButton component tested

---
+++ src/components/Buttons/PaginationButton.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import PaginationButton from "./PaginationButton";
+
+describe("Given a PaginationButton component", () => {
+  describe("When it receives an action and the button is clicked", () => {
+    test("Then it should call the action", () => {
+      const action = jest.fn();
+
+      render(<PaginationButton actionOnClick={action} text="text" />);
+
+      userEvent.click(screen.getByRole("button"));
+
+      expect(action).toHaveBeenCalled();
+    });
+  });
+});
