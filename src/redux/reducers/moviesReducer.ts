21ba3ab7a23c1626c96daa734c3779bd5f34fe9c
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-11 20:03:17 +0100

    moviesReducer delete created

--- src/redux/reducers/moviesReducer.ts
+++ src/redux/reducers/moviesReducer.ts
@@ -1,14 +1,31 @@
-import { LoadLocalMoviesActionInterface, Movie } from "../../interfaces/Movie";
+import {
+  LoadLocalMoviesActionInterface,
+  DeleteLocalMovieActionInterface,
+  Movie,
+  SomeActionInterface,
+} from "../../interfaces/Movie";
 import { actionsTypes } from "../actions/actionsTypes";
 
 const moviesReducer = (
   currentMovies: Movie[] = [],
-  action: LoadLocalMoviesActionInterface = { type: "", movies: [] }
+  action:
+    | SomeActionInterface
+    | LoadLocalMoviesActionInterface
+    | DeleteLocalMovieActionInterface = {
+    type: "",
+    movies: [],
+  }
 ): Movie[] => {
   let newMovies: Movie[];
   switch (action.type) {
     case actionsTypes.loadLocalMovies:
-      newMovies = [...action.movies];
+      newMovies = [...(action as LoadLocalMoviesActionInterface).movies];
+      break;
+    case actionsTypes.deleteLocalMovie:
+      newMovies = currentMovies.filter(
+        (movie) =>
+          movie._id !== (action as DeleteLocalMovieActionInterface).movieId
+      );
       break;
 
     default:
