f1f52edacebfa91827ca4cc68da735c7ed4af9c1
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-18 10:35:43 +0100

    loginUserThunk created

--- src/redux/thunks/usersThunk.ts
+++ src/redux/thunks/usersThunk.ts
@@ -1,8 +1,12 @@
-// import jwtDecode from "jwt-decode";
+import jwtDecode from "jwt-decode";
 import { AnyAction } from "redux";
 import { ThunkDispatch } from "redux-thunk";
-import { userRegisterDataInterface } from "../../interfaces/User";
 import {
+  userLoginDataInterface,
+  userRegisterDataInterface,
+} from "../../interfaces/User";
+import {
+  loginSuccessAction,
   registerFailAction,
   registerSuccessAction,
   setMessageAction,
@@ -28,5 +32,33 @@ export const createUserThunk =
     } else {
       dispatch(registerSuccessAction());
       dispatch(setMessageAction(`${newUser.username} registered!`));
+    }
+  };
+
+export const loginUserThunk =
+  (userLoginData: userLoginDataInterface) =>
+  async (dispatch: ThunkDispatch<RootState, void, AnyAction>) => {
+    const response = await fetch(`${url}user/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userLoginData),
+    });
+    if (response.ok) {
+      const tokenResponse = await response.json();
+      const { id, username }: any = await jwtDecode(tokenResponse.token);
+      localStorage.setItem(
+        "userToken",
+        JSON.stringify({
+          id,
+          username,
+          token: tokenResponse.token,
+        })
+      );
+      dispatch(
+        loginSuccessAction({ id, username, token: tokenResponse.token })
+      );
+    } else {
     }
   };
