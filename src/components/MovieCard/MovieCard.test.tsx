98bf9988f9c1135a9038f26fe796bac151f48b3c
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-22 21:11:52 +0100

    movieCard tested

--- src/components/MovieCard/MovieCard.test.tsx
+++ src/components/MovieCard/MovieCard.test.tsx
@@ -1,8 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "../../redux/store";
 import { MovieCard } from "./MovieCard";
+
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
 
 describe("Given a MovieCard component", () => {
   describe("When it is receives a movie", () => {
@@ -27,8 +34,8 @@ describe("Given a MovieCard component", () => {
       expect(title).toBeInTheDocument();
     });
   });
-  describe("When it is receives a movie with a title longer than 20 characters", () => {
-    test("Then it should display the first 19 characters of the title followed by ...", () => {
+  describe("When it is receives a movie with a title longer than 14 characters", () => {
+    test("Then it should display the first 13 characters of the title followed by ...", () => {
       const movie = {
         Title: "HelloHelloHelloHelloHelloHello",
         Year: "1999",
@@ -49,6 +56,35 @@ describe("Given a MovieCard component", () => {
       });
 
       expect(title).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is receives a movie with a title longer than 20 characters", () => {
+    test("Then it should display the first 19 characters of the title followed by ...", () => {
+      const movie = {
+        Title: "HelloHelloHelloHelloHelloHello",
+        Year: "1999",
+        Type: "Movie",
+        Poster: "",
+        _id: "6228796b92d232f647b99044",
+      };
+      render(
+        <BrowserRouter>
+          <Provider store={store}>
+            <MovieCard isLoggedIn={true} movie={movie} />
+          </Provider>
+        </BrowserRouter>
+      );
+
+      const buttons = screen.getAllByRole("button");
+      const images = screen.getAllByRole("img");
+      userEvent.click(images[0]);
+      // console.log(buttons.length);
+      // console.log(buttons[1]);
+      // userEvent.click(buttons[0]);
+      expect(buttons).toHaveLength(3);
+
+      expect(mockNavigate).toHaveBeenCalled();
     });
   });
 });
