049cd39319865ab34d556810b755bec1ed1b2378
MarcGiroPiraces <marc.giro.piraces@gmail.com>
2022-03-23 20:42:29 +0100

    cypress test updated

--- cypress/integration/app-spec.ts
+++ cypress/integration/app-spec.ts
@@ -6,12 +6,12 @@ describe("App", function () {
     cy.visit("/login");
   });
 
-  it("should find the 1st input and type marc", function () {
+  it("should find the 1st input and type marc and the second input and type 1234", function () {
     cy.get("input").first().type("marc").should("have.value", "marc");
     cy.get("input").last().type("1234").should("have.value", "1234");
   });
 
-  it("should find the 2nd input and type 1234", function () {
+  it("should click the button and log in", function () {
     cy.get("button").click;
   });
 });
